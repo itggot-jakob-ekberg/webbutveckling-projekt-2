5f2dc1ce5c8e19d32254b4ef2e60f9f4fe52d5d6
itggot-jakob-ekberg <jakob.ekberg@elev.ga.ntig.se>
2019-01-26 13:19:13 +0100

    Lagt till: Meny som är klickbar och förlänger ut. Sedan kommer stå streck ner(linjer.png), sedan kommer meny alternativen med hjälp av cubic bezier, ändrat ikonen efter click, Lagt till 2 nya sidor history och about. Lagt till lådor för texterna som ska vara på about och history, Lagt till test bilder till lådorna där historien ska stå

--- site/JS/main.js
+++ site/JS/main.js
@@ -1,4 +1,23 @@
 function menu1(event){
-    let header = document.querySelector(".menubox")
-    header.classList.toggle("activate")
+    let hea = document.querySelector("header")
+    hea.classList.toggle("activate")
+
+    let lines = document.querySelector(".menulines")
+    lines.classList.toggle("linedown")
+    
+    let hom = document.querySelector(".Menubox1")
+    hom.classList.toggle("ho")
+
+    let histor = document.querySelector(".Menubox2")
+    histor.classList.toggle("hi")
+
+    let abo = document.querySelector(".Menubox3")
+    abo.classList.toggle("ab")
+
+    let icon = document.querySelector("#menubutton")
+    if(icon.innerHTML == "close"){
+        icon.innerHTML = "menu"
+    }else{
+        icon.innerHTML = "close"
+    }
 }
